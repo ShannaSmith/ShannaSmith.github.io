c6e063726ab7fe1f07afe84a494c1e54a600881c
Shanna Smith <87396782+ShannaSmith@users.noreply.github.com>
2022-04-24 22:38:49 -0400

    comment out video

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-let tag = document.createElement('script');
+// // 2. This code loads the IFrame Player API code asynchronously.
+// let tag = document.createElement('script');
   
-tag.src = "https://www.youtube.com/iframe_api";
-let firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// let firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-let player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    width: '50%',
-    videoId: 'MqexQN6UvFw',
-    playerVars: { 'autoplay': 0, 
-    'playsinline': 1,
-    'controls': 0,
-    'showinfo':0,
-    'modestbranding': 1,
-    'loop': 1,
-    'fs': 0,
-    'cc load_policty': 1,
-    'iv_load_policy': 3,
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// let player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     width: '50%',
+//     videoId: 'MqexQN6UvFw',
+//     playerVars: { 'autoplay': 0, 
+//     'playsinline': 1,
+//     'controls': 0,
+//     'showinfo':0,
+//     'modestbranding': 1,
+//     'loop': 1,
+//     'fs': 0,
+//     'cc load_policty': 1,
+//     'iv_load_policy': 3,
     
-     },
-    events: {
-      'onReady': function (e) {
-          //e.target.mute();
-          e.target.setVolume(50);
-      }
-    }
-  });
-}
+//      },
+//     events: {
+//       'onReady': function (e) {
+//           //e.target.mute();
+//           e.target.setVolume(50);
+//       }
+//     }
+//   });
+// }
 
-// 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-   //e.target.mute();
-   e.target.setVolume();
-  e.target.playVideo();
-}
+// // 4. The API will call this function when the video player is ready.
+//   function onPlayerReady(event) {
+//    //e.target.mute();
+//    e.target.setVolume();
+//   e.target.playVideo();
+// }
 
  
